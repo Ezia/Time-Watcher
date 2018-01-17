8d1a63f8bb757a7c72cd988cbdc7e781f1a7c7a5
Ezia <esia.belbachir@protonmail.com>
2018-01-17 12:01:25 -0500

    Create 2 states for a time edit text : now or a given time

--- app/src/main/java/esia/timewatcher/view/TimeEditText.java
+++ app/src/main/java/esia/timewatcher/view/TimeEditText.java
@@ -2,6 +2,7 @@ package esia.timewatcher.view;
 
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.widget.AppCompatEditText;
 import android.text.InputType;
 import android.util.AttributeSet;
@@ -11,16 +12,16 @@ import org.joda.time.DateTime;
 import esia.timewatcher.utils.TimeUtils;
 
 public class TimeEditText extends AppCompatEditText {
-	private int minutes;
-	private int hours;
-	private int seconds;
+	private int minutes = 0;
+	private int hours = 0;
+	private int seconds = 0;
+	private boolean now;
 
 	private void init() {
 		setFocusable(false);
 		setInputType(InputType.TYPE_NULL);
 		setOnClickListener(view -> onClick());
-		DateTime now = DateTime.now();
-		setTime(now.getHourOfDay(), now.getMinuteOfHour(), now.getSecondOfMinute());
+		setTimeNow();
 	}
 
 	public TimeEditText(Context context) {
@@ -38,7 +39,13 @@ public class TimeEditText extends AppCompatEditText {
 		init();
 	}
 
+	public void setTimeNow() {
+		now = true;
+		setText("now");
+	}
+
 	public void setTime(int hours, int minutes, int seconds) {
+		now = false;
 		this.hours = hours;
 		this.minutes = minutes;
 		this.seconds = seconds;
@@ -47,20 +54,48 @@ public class TimeEditText extends AppCompatEditText {
 	}
 
 	public int getMinutes() {
-		return minutes;
+		if (now) {
+			return DateTime.now().getMinuteOfHour();
+		} else {
+			return minutes;
+		}
 	}
 
 	public int getHours() {
-		return hours;
+		if (now) {
+			return DateTime.now().getHourOfDay();
+		} else {
+			return hours;
+		}
 	}
 
 	public int getSeconds() {
-		return seconds;
+		if (now) {
+			return DateTime.now().getSecondOfMinute();
+		} else {
+			return seconds;
+		}
 	}
 
 	public void onClick() {
-		new TimePickerDialog(getContext(),
-				(timePicker, h, m) -> setTime(h, m, 0),
-				hours, minutes, true).show();
+
+		TimePickerDialog dialog;
+		if (now) {
+			DateTime nowTime = DateTime.now();
+			dialog = new TimePickerDialog(getContext(),
+					(timePicker, h, m) -> setTime(h, m, 0),
+					nowTime.getHourOfDay(),
+					nowTime.getMinuteOfHour(), true);
+		} else {
+			dialog = new TimePickerDialog(getContext(),
+					(timePicker, h, m) -> setTime(h, m, 0),
+					hours, minutes, true);
+		}
+		dialog.setButton(
+				TimePickerDialog.BUTTON_NEUTRAL,
+				"Reset",
+				(dialog1, which) -> setTimeNow()
+		);
+		dialog.show();
 	}
 }
